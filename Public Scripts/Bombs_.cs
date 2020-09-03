e8b4c83232f4a6366bbea324e5fb1498de035598
3Hearts <48471582+3Hearts@users.noreply.github.com>
2020-09-03 19:03:04 -0400

    Update Bombs_.cs

--- Public Scripts/Bombs_.cs
+++ Public Scripts/Bombs_.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 public class Bombs_ : MonoBehaviour
 {
-
     public GameObject tnt;
     private PlayerController playercontroller;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         playercontroller = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (playercontroller.tntpack >= 1 && Input.GetKeyDown("space"))
         {
-            print("space key was pressed");
+            //print("space key was pressed");
 
             Instantiate(tnt, transform.position, transform.rotation);
 
             playercontroller.tntpack = playercontroller.tntpack - 1;
             playercontroller.GetComponent<PlayerController>().UpdateTNTPack();
-
-            
         }
     }
 }
