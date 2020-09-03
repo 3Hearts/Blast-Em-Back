81aeed70107349b82cf90900204173dcfd9ede9e
3Hearts <48471582+3Hearts@users.noreply.github.com>
2020-09-03 18:59:45 -0400

    Add files via upload

---
+++ Public Scripts/Bombs_.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bombs_ : MonoBehaviour
+{
+
+    public GameObject tnt;
+    private PlayerController playercontroller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playercontroller = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playercontroller.tntpack >= 1 && Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+
+            Instantiate(tnt, transform.position, transform.rotation);
+
+            playercontroller.tntpack = playercontroller.tntpack - 1;
+            playercontroller.GetComponent<PlayerController>().UpdateTNTPack();
+
+            
+        }
+    }
+}
---
+++ Public Scripts/BreakableScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableScript : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        DisableRagdoll();
+    }
+
+    // Let the rigidbody take control and detect collisions.
+    void EnableRagdoll()
+    {
+        rb.isKinematic = false;
+      //  rb.detectCollisions = true;
+    }
+
+    // Let animation control the rigidbody and ignore collisions.
+    void DisableRagdoll()
+    {
+        rb.isKinematic = true;
+       // rb.detectCollisions = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Explosive")
+        {
+            StartCoroutine(Breakabledeath());
+        }
+    }
+
+    IEnumerator Breakabledeath()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(this.gameObject);
+    }
+}
